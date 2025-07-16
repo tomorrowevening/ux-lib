d6bfd7f9d4ee3d30964ca59af0e1cfc1cf45cb9e
Colin Duffy <colin@tomorrowevening.com>
2025-07-15 18:54:56 -0700

    fix(dev): mode added

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const isDev = import.meta.env.MODE == 'development'
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    {isDev ? (
+      <>
+        <App />
+      </>
+    ) : (
+      <StrictMode>
+        <App />
+      </StrictMode>
+    )}
+  </>,
 )
